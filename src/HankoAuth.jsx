8754169e1bc0d90bf840dc0798154e3b29d0ebc4
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2023-10-15 15:25:18 +0300

    Update HankoAuth.jsx changing most part of the code

--- src/HankoAuth.jsx
+++ src/HankoAuth.jsx
@@ -1,30 +1,31 @@
-import React, { useCallback, useMemo } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+  useMemo,
+} from "react";
 import { useNavigate } from "react-router-dom";
-import { register, Hanko } from "@teamhanko/hanko-elements";
+import { Hanko, register } from "@teamhanko/hanko-elements";
 
 const hankoApi = import.meta.env.VITE_BASE_URL;
 
 const Login = () => {
   const navigate = useNavigate();
-  const hanko = useMemo(() => new Hanko(hankoApi), []);
+  const [error, setError] = useState(null);
+  const hankoClient = useMemo(() => new Hanko(api), []);
 
   const redirectAfterLogin = useCallback(() => {
-    navigate("/home", {replace: true});
+    navigate("/home", { replace: true });
   }, [navigate]);
 
-  React.useEffect(
-    () =>
-      hanko.onAuthFlowCompleted(() => {
-        redirectAfterLogin();
-      }),
-    [hanko, redirectAfterLogin]
-  );
-
-  React.useEffect(() => {
-    register(hankoApi).catch((error) => {
-      alert(error)
-    });
+  useEffect(() => {
+    register(api).catch(setError);
   }, []);
+
+  useEffect(
+    () => hankoClient.onAuthFlowCompleted(() => redirectAfterLogin()),
+    [hankoClient, redirectAfterLogin]
+  );
 
     return (
        <div className="flex min-h-screen justify-center items-center bg-neutral-900">
