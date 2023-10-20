a293f678abcab2ea3dbfa3d3b6c505e30587b926
Ciamuthama <muthamapeter79@gmail.com>
2023-10-20 21:57:56 +0300

    Fixed navigation after login bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./HankoAuth";
 import Home from "./home";
--- src/HankoAuth.jsx
+++ src/HankoAuth.jsx
@@ -3,13 +3,13 @@ import React, {
   useState,
   useMemo,
 } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { Hanko, register } from "@teamhanko/hanko-elements";
 
 const hankoApi = import.meta.env.VITE_BASE_URL;
 
 const Login = () => {
-  const navigate = useNavigate();
+  
   const [error, setError] = useState(null);
   const hankoClient = useMemo(() => new Hanko(hankoApi), []);
 
@@ -21,8 +21,9 @@ const Login = () => {
       if (!localStorage.getItem("u_id")) {
           localStorage.setItem("u_id", generateUserID());
       }
-      navigate("/home", {replace:true});
-  }, [navigate]);
+      Navigate("/home", {replace:true});
+      <Navigate to={"/home"}/>
+  }, []);
   
   useEffect(() => {
     register(hankoApi).catch(setError);
