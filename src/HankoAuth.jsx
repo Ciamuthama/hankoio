0dfe6910c6ff0d32dd23bf0af2dc420be9223d52
Ciamuthama <muthamapeter79@gmail.com>
2023-10-28 13:29:11 +0300

    Deleted unused code in the Auth component

--- src/HankoAuth.jsx
+++ src/HankoAuth.jsx
@@ -5,7 +5,7 @@ import React, {
 import { useNavigate } from "react-router-dom";
 import { Hanko, HankoError, register } from "@teamhanko/hanko-elements";
 import { useGlobalContext } from "./constant";
-import Home from "./home";
+
 const hankoApi = import.meta.env.VITE_BASE_URL;
 
 const Login = () => {
@@ -14,24 +14,14 @@ const Login = () => {
   const hankoClient = useMemo(() => new Hanko(hankoApi), []);
   const userID = () => Math.random().toString(36).substring(2, 10);
 
-  const {active} = useGlobalContext()
-
-  const switcher=() => {
-    switch (active) {
-      case "Home":
-        return <Home />;
-      default:
-        null;
-    }
-  }
-
+  
   const redirectAfterLogin = React.useCallback(() => {
       localStorage.setItem("loggedIn", "true");
       if (!localStorage.getItem("u_id")) {
           localStorage.setItem("u_id", userID());
       }
     navigate(url);
-    // navigate(switcher)
+    
   }, [navigate, url]);
   
   useEffect(() => {
