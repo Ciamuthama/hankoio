a71f3dab185e811286b42f6a0775545c73635cd7
Ciamuthama <muthamapeter79@gmail.com>
2023-10-28 13:06:42 +0300

    Added a constant to take care of switches  between login and home screens

---
+++ src/constant.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Hanko } from '@teamhanko/hanko-elements'
+
+const AppContext = React.createContext()
+
+// eslint-disable-next-line react/prop-types
+export const Context=({ children }) => {
+    const [active, setActive] = React.useState('Home')
+    const [url, setUrl] = React.useState('/home')
+
+    const apiUrl = import.meta.env.VITE_BASE_URL;
+    const hanko = new Hanko(apiUrl)
+
+    const session = hanko.session.get()
+
+    const user = () => session && session.userID
+    
+    const getUser = () => {
+        return JSON.parse(localStorage.getItem(`userID()`))
+    }
+
+
+    return (
+        <AppContext.Provider
+            value={{
+                active,
+                url,
+                setActive,
+                setUrl,
+                user,
+                getUser,
+        }}
+        >
+            { children}
+        </AppContext.Provider>
+    )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export  const useGlobalContext = () => {
+    return React.useContext(AppContext)
+}
