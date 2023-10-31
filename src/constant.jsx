785c46cb5bbcaa87f76c6088680b14e3b83eaa5d
Ciamuthama <muthamapeter79@gmail.com>
2023-11-01 00:05:19 +0300

    Fixed some final final bugs ;)

--- src/constant.jsx
+++ src/constant.jsx
@@ -7,7 +7,7 @@ const AppContext = React.createContext()
 export const Context=({ children }) => {
     const [active, setActive] = React.useState('Home')
     const [url, setUrl] = React.useState('/')
-    const [urlLogin, setUrlLogin] = React.useState('/')
+    const [urlLogin, setUrlLogin] = React.useState('/login')
 
     const apiUrl = import.meta.env.VITE_BASE_URL;
     const hanko = new Hanko(apiUrl)
